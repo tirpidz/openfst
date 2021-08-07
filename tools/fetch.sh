7e78edf8a8615770e7c6080ffa2e14f5cc8f96f1
Martin Turcotte <tirpidz@gmail.com>
2021-08-07 19:23:22 -0400

    chore(tools): fixes

--- tools/fetch.sh
+++ tools/fetch.sh
@@ -10,19 +10,19 @@ set -euo pipefail
 # This setting prevents errors in a pipeline from being masked. If any command in a pipeline fails,
 # that return code will be used as the return code of the whole pipeline.
 
-REPOSITORY_DIRECTORY="~/src/openfst"
+REPOSITORY_DIRECTORY="${HOME}/src/openfst"
 
 fetch_openfst() {
     VERSION_NUMBER=$1
     REVISION_NUMBER=$2
 
     cd ${REPOSITORY_DIRECTORY}
-    wget "http://www.openfst.org/twiki/bin/viewfile/FST/FstDownload?filename=openfst-${VERSION_NUMBER}.tar.gz;rev=${REVISION_NUMBER}"
     rm -rf ${REPOSITORY_DIRECTORY}/OpenFst*
+    wget "http://www.openfst.org/twiki/bin/viewfile/FST/FstDownload?filename=openfst-${VERSION_NUMBER}.tar.gz;rev=${REVISION_NUMBER}" -O OpenFst-v${VERSION_NUMBER}-r${REVISION_NUMBER}.tar.gz
     mkdir OpenFst
     tar -xf *gz -C OpenFst --strip-components 1
     git add .
-    git commit "feat(OpenFst): ${VERSION_NUMBER} rev ${REVISION_NUMBER}"
+    git commit -am "feat(OpenFst): ${VERSION_NUMBER} rev ${REVISION_NUMBER}"
     git tag -am v${VERSION_NUMBER}-r${REVISION_NUMBER} "openfst-v${VERSION_NUMBER}-r${REVISION_NUMBER}"
     git push --tags
 }
